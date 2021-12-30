9f05c9c8e18f3ab774d5bc18a6e17be87a452ed9
German Reinke <german.reinke@web.de>
2021-12-30 19:35:13 +0100

    Initial Commit

---
+++ Library.DataAccess/IAbstractRepository.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq.Expressions;
+
+namespace Library.DataAccess;
+
+public interface IAbstractRepository<TEntity> where TEntity : class
+{
+    public TEntity Get(string id);
+
+    public ICollection<TEntity> GetMany(ICollection<string> ids);
+
+    public ICollection<TEntity> GetAll();
+
+    public ICollection<TEntity> Filter(Expression<Func<TEntity, bool>> filterExpression);
+
+    public TEntity Create(TEntity entity);
+
+    public ICollection<TEntity> CreateMany(ICollection<TEntity> entity);
+
+    public TEntity Update(TEntity entity);
+
+    public ICollection<TEntity> UpdateMany(ICollection<TEntity> entity);
+
+    public void Delete(TEntity entity);
+
+    public void DeleteMany(ICollection<TEntity> entities);
+}
